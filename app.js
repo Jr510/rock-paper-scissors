de857527f9827b55ddc41e9d70b71f7a728fa01b
Jr510 <plepag3jr1265@gmail.com>
2022-06-22 22:21:21 -0400

    Add playRound function to return outcome between Player and Computer

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 // Randomly return "Rock", "Paper", or "Scissors" for Computer
 
 function computerPlay() {
-  let cpuNumber  = Math.floor(Math.random() * 3) + 1;
+  const cpuNumber  = Math.floor(Math.random() * 3) + 1;
 
   if(cpuNumber === 1) {
-    console.log("Rock");
+    return "Rock";
   } else if(cpuNumber === 2) {
-    console.log("Paper");
-  } else {
-    console.log("Scissors");
+    return "Paper";
+  } else if (cpuNumber === 3) {
+    return "Scissors";
   }
 }
 
+// Player against Computer and return outcome
+
+function playRound(playerSelection, computerSelection) {
+
+  const playerChoice = playerSelection.toLowerCase();
+  const newPlayerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+  computerSelection = computerPlay();
+
+  if(newPlayerChoice === computerSelection) {
+    console.log(`It's a tie!  ${newPlayerChoice} and ${computerSelection}`);
+
+  } else if (newPlayerChoice === "Rock" && computerSelection === "Paper") {
+    console.log(`Computer Wins!  ${computerSelection} covers ${newPlayerChoice}!`);
+
+  } else if (newPlayerChoice === "Paper" && computerSelection === "Rock") {
+    console.log(`Player Wins!  ${newPlayerChoice} covers ${computerSelection}!`);
+
+  } else if (newPlayerChoice === "Scissors" && computerSelection === "Rock") {
+    console.log(`Computer Wins!  ${computerSelection} breaks ${newPlayerChoice}!`);
+
+  } else if (newPlayerChoice === "Rock" && computerSelection === "Scissors") {
+    console.log(`Player Wins!  ${newPlayerChoice} breaks ${computerSelection}!`);
+
+  } else if (newPlayerChoice === "Paper" && computerSelection === "Scissors") {
+    console.log(`Computer Wins!  ${computerSelection} cuts ${newPlayerChoice}!`);
+
+  } else if (newPlayerChoice === "Scissors" && computerSelection === "Paper") {
+    console.log(`Player Wins!  ${computerSelection} cuts ${newPlayerChoice}!`);
+  }
+
+}
